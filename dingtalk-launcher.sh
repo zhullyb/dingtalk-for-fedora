90e0e8061a244e8665b0dd6e1c57e2fa580295b9
Kelatte <kelatte@outlook.com>
2024-09-28 13:35:00 +0800

    Update dingtalk-launcher.sh
    comment LD_LIBRARY, to fix "/lib64/libpango-1.0.so.0: undefined symbol: hb_ot_metrics_get_position"

--- dingtalk-launcher.sh
+++ dingtalk-launcher.sh
@@ -2,6 +2,6 @@
 
 cd /opt/dingtalk-bin/*Release*/
 export PATH=$(pwd):$PATH
-export LD_LIBRARY_PATH=$(pwd):$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=$(pwd):$LD_LIBRARY_PATH
 export QT_QPA_PLATFORM="wayland;xcb"
 ./com.alibabainc.dingtalk $1
